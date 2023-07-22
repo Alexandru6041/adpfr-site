4f20593528bf9e5f509249f2f062797ad5b25ab7
Alex <alexchelariu834@gmail.com>
2023-07-22 21:08:03 +0300

    landing-page v2.0(contact_cards_added) + licenta_reproducere page

--- main/static/js/index/index-navbar.js
+++ main/static/js/index/index-navbar.js
@@ -1,8 +1,7 @@
+import ChangeThemeClass from "../utils/ThemeMode.js";
 
 
-import CookieController from "../utils/CookieManager.js";
-
-const cookie_expire = 2913342; ///days
+const cookie_expire = 2913342; ///Measured in days
 const cookie_changeTheme_name = "theme";
 
 (function($) 
@@ -29,14 +28,14 @@ const cookie_changeTheme_name = "theme";
 			});
 		});		
 		
-		//Animation
+		///Animation
 		
 		$(document).ready(function() 
 		{
 			$('body.hero-anime').removeClass('hero-anime');
 		});
 
-		//Menu On Hover
+		///Menu On Hover
 			
 		$('body').on('mouseenter mouseleave','.nav-item',function(e)
 		{
@@ -49,38 +48,10 @@ const cookie_changeTheme_name = "theme";
 				}
 		});	
 		
-		//Switch light/dark
+		///Switch light/dark
 
-		let Theme_Cookie_Control = new CookieController(cookie_changeTheme_name);
-		console.log(Theme_Cookie_Control)
-		
-		$("#switch").on('click', function () {
-			
+		let Theme_Controller = new ChangeThemeClass(cookie_changeTheme_name, cookie_expire);
+		Theme_Controller.run_changeTheme();
 
-			if ($("body").hasClass("dark")) 
-			{
-				$("body").removeClass("dark");
-				$("#switch").removeClass("switched");
-				var ok = Theme_Cookie_Control.getCookie();
-				if(ok)
-				{
-					Theme_Cookie_Control.eraseCookie();
-				}
-
-				Theme_Cookie_Control.setCookie("light", cookie_expire);
-			}
-
-			else {
-				$("body").addClass("dark");
-				$("#switch").addClass("switched");
-				var ok = Theme_Cookie_Control.getCookie();
-				if(ok)
-				{
-					Theme_Cookie_Control.eraseCookie();
-				}
-
-				Theme_Cookie_Control.setCookie("dark", cookie_expire)
-			}
-		});  
 	
   })(jQuery);
