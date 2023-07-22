3164abc1187bc7d5c649616572c751dd95554f59
Alex <alexchelariu834@gmail.com>
2023-07-22 23:17:00 +0300

    Empty Legal Pages --> Text Formatting

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name = "index"),
     path("licenta-reproducere/", views.licenta_reproducere, name = "licenta-reproducere"),
+    path("legislatie/", views.legislatie, name = "legislatie"),
+    path("politica-de-confidentialitate/", views.privacy_policy, name = "privacy-policy"),
+    path("termeni-si-conditii/", views.terms_and_conditions, name = "terms-and-conditions"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/views.py
+++ main/views.py
@@ -19,3 +19,24 @@ def licenta_reproducere(request):
     context = {Theme_CookieName : Theme_CookieValue}
     
     return render(request, "ro/Legal/licenta-reproducere.html", context)
+
+def legislatie(request):
+    Theme_CookieValue = request.COOKIES.get(Theme_CookieName)
+    
+    context = {Theme_CookieName : Theme_CookieValue}
+    
+    return render(request, "ro/Legal/legislatie.html", context)
+
+def privacy_policy(request):
+    Theme_CookieValue = request.COOKIES.get(Theme_CookieName)
+    
+    context = {Theme_CookieName : Theme_CookieValue}
+    
+    return render(request, "ro/Legal/privacy-policy.html", context)
+
+def terms_and_conditions(request):
+    Theme_CookieValue = request.COOKIES.get(Theme_CookieName)
+    
+    context = {Theme_CookieName : Theme_CookieValue}
+    
+    return render(request, "ro/Legal/terms-and-conditions.html", context)
