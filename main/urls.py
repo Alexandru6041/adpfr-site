d0b79d0b09b680c2bcdea9c1328aa7cf3ac16170
Alex <alexchelariu834@gmail.com>
2023-08-16 20:19:21 +0300

    Revert "Revert "about-us pages v1.0 *blank*""
    This reverts commit 41ec4c64999ec33fb0d8910c05aa416feacbc476.

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
     path("legislatie/", views.legislatie, name = "legislatie"),
     path("politica-de-confidentialitate/", views.privacy_policy, name = "privacy-policy"),
     path("termeni-si-conditii/", views.terms_and_conditions, name = "terms-and-conditions"),
+    path("cine-suntem/", views.cine_suntem , name = "cine_suntem"),
+    path("ce-facem/", views.ce_facem , name = "ce_facem"),
+    path("comisii/", views.comisii , name = "comisii"),
+    path("contact/", views.contact , name = "contact"),
+    path("management-si-structura/", views.management , name = "management-si-structura"),
+    path("membri/", views.membri , name = "membri"),
+    path("statutul-adpfr/", views.statut , name = "Statutul-ADPFR"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
